4ff8d13d9ece82ee2b5624d278fb009c3f8238cd
Ahmet <1yigitbilici@gmail.com>
2023-09-17 13:38:50 -0500

    Set page title, icon, and desc

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/Ahmet1Bilici")}
+          className="black-btn"
+        >
+          github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAi GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with SumzText, an open-sourxe text summarizer that
+        transforms length texts into clear and concise summaries for your
+        advantage.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
